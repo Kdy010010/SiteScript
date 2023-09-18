34e3733f28f1ff87d1bbb73e20ec832a2597cfaf
Kdy010010 <137839917+Kdy010010@users.noreply.github.com>
2023-09-19 08:20:03 +0900

    Update Main.js

--- Main.js
+++ Main.js
@@ -1,14 +1,27 @@
-function variable(a,b)
-return let a = "b";
-function constant(c,d)
-return const c = "d";
-function string(e)
-return let string = "e";
-function number(f)
-return let number = "f";
-function boolean(g)
-return let boolean = "g";
-function object(h,i)
-return let obj = {h: "i"}
-function array(array)
-let array = [array];
+// 1. getVariable(a, b)
+let variableResult = getVariable("x", "y");
+// variableResult now holds the value "x = "y""
+
+// 2. getConstant(c, d)
+let constantResult = getConstant("alpha", "beta");
+// constantResult now holds the value "alpha = "beta""
+
+// 3. getStringValue(e)
+let stringResult = getStringValue("Hello");
+// stringResult now holds the value "Hello"
+
+// 4. getNumberValue(f)
+let numberResult = getNumberValue(123);
+// numberResult now holds the value "123"
+
+// 5. getBooleanValue(g)
+let booleanResult = getBooleanValue(true);
+// booleanResult now holds the value "true"
+
+// 6. getObjectValue(h, i)
+let objectResult = getObjectValue("name", "John");
+// objectResult now holds the value "{name: "John"}"
+
+// 7. getArrayValue(arrayItem)
+let arrayResult = getArrayValue("apple");
+// arrayResult now holds the value ["apple"]
