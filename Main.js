e911a2361f58be822c2dd3adb136f01b3e77e3be
Kdy010010 <137839917+Kdy010010@users.noreply.github.com>
2023-09-18 22:35:31 +0900

    Update Main.js

--- Main.js
+++ Main.js
@@ -5,6 +5,10 @@ return const c = "d";
 function string(e)
 return let string = "e";
 function number(f)
-return let number = f;
+return let number = "f";
 function boolean(g)
-return let boolean = g;
+return let boolean = "g";
+function object(h,i)
+return let obj = {h: "i"}
+function array(array)
+let array = [array];
