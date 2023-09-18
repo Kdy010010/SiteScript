0c9eb5078bea5d28d6f0df835fac743774d54e0b
Kdy010010 <137839917+Kdy010010@users.noreply.github.com>
2023-09-18 22:29:38 +0900

    Create Main.js

---
+++ Main.js
@@ -0,0 +1,10 @@
+function variable(a,b)
+return let a = "b";
+function constant(c,d)
+return const c = "d";
+function string(e)
+return let string = "e";
+function number(f)
+return let number = f;
+function boolean(g)
+return let boolean = g;
